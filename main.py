377226859c1593b6115c6139f54ecf7942867585
zhiyuan <1005897034@qq.com>
2022-06-26 17:48:16 +0800

    basic build

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-class Config:
-    FILE_PATH = r"./simples/10Hz.mp4"    # 待处理视频路径
-    SAVE_RESULT = True                   # 是否保存结果至本地txt文件
-    LEFT_TOP = [500, 600]                # 采样区域左上角坐标
-    RIGHT_BOTTOM = [800, 900]            # 采样区域右下角坐标
-    BORDER_COLOR = [0, 0, 255]           # 边框颜色(BGR)
-
-
 def parse_opt():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--path', default='./simples/20Hz.mp4', help='视频文件路径')
+    parser.add_argument('--save', type=bool, default=True, help='是否将检测结果保存至本地txt文件')
+    parser.add_argument('--left_top', type=list, default=[500, 600], help='左上角顶点坐标')
+    parser.add_argument('--right-bottom', type=list, default=[800, 900], help='右下角顶点坐标')
+    parser.add_argument('--color', type=list, default=[0, 0, 255], help='边框颜色(BGR)')
+    opt = parser.parse_args()
+    return opt
 
 
 def get_avg_gray_value(img):
@@ -23,10 +22,10 @@ def get_avg_gray_value(img):
     return means[0, 0]
 
 
-def save2local(x_pts, y_pts, cfg: Config):
+def save2local(x_pts, y_pts, opt):
     print("\n===========SAVING TO LOCAL===========")
     # 生成保存路径
-    file_name = os.path.splitext(os.path.split(cfg.FILE_PATH)[-1])[0]
+    file_name = os.path.splitext(os.path.split(opt.path)[-1])[0]
     save_path = os.path.join('./', file_name+'_result.txt')
 
     # 判断结果文件是否存在
@@ -35,7 +34,7 @@ def save2local(x_pts, y_pts, cfg: Config):
         if os.path.isfile(save_path):
             print_info("The result file already exists, do you want to overwrite it?", 1)
             if not input("enter 'y' to confirm: ") is 'y':
-                print_info("Results not saved!", 0)
+                print_info("Results not saved!", 1)
                 return
     except FileNotFoundError:
         pass
@@ -62,24 +61,22 @@ def print_info(info, info_type):
     print(info)
 
 
-def main():
-    # 实例化配置类
-    myconfig = Config()
+def main(opt):
     # 待绘制点集
     x_points = []
     y_points = []
 
     # 读取视频文件并进行处理
-    capture = cv.VideoCapture(myconfig.FILE_PATH)
+    capture = cv.VideoCapture(opt.path)
     frame_count = 0
     while capture.isOpened():
         ret, frame = capture.read()
         if ret:
             # 读取坐标
-            x_min = myconfig.LEFT_TOP[0]
-            x_max = myconfig.RIGHT_BOTTOM[0]
-            y_min = myconfig.LEFT_TOP[1]
-            y_max = myconfig.RIGHT_BOTTOM[1]
+            x_min = opt.left_top[0]
+            x_max = opt.right_bottom[0]
+            y_min = opt.left_top[1]
+            y_max = opt.right_bottom[1]
 
             # 对图像进行深度复制
             copied = np.empty_like(frame)
@@ -95,7 +92,7 @@ def main():
             print(gray_means)
 
             # 兴趣区域可视化
-            cv.rectangle(frame, myconfig.LEFT_TOP, myconfig.RIGHT_BOTTOM, myconfig.BORDER_COLOR, 3)
+            cv.rectangle(frame, opt.left_top, opt.right_bottom, opt.color, 3)
             frame = cv.resize(frame, (0, 0), fx=0.5, fy=0.5)
             cv.imshow("frame", frame)
             if cv.waitKey(1) & 0xFF == ord('q'):
@@ -108,7 +105,7 @@ def main():
     capture.release()
     cv.destroyAllWindows()
     # 将结果保存至本地
-    save2local(x_points, y_points, myconfig)
+    save2local(x_points, y_points, opt)
     # 折线图绘制
     print("\n===========SHOW THE GRAPH============")
     print_info("Chart displayed", 0)
@@ -117,6 +114,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    options = parse_opt()
+    main(options)
 
 
